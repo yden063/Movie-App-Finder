487a5b276c7038526e6804f17194d4b663b8c670
Youness Dendane <youness.dendane1@uqac.ca>
2019-05-08 11:58:41 -0400

    Added API searching and minimal results displaying

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+'use strict';
+
 $(document).ready(() => {
-  
+  $('#searchForm').on('submit', (e) => {
+    e.preventDefault();
+    let searchText = $('#searchText').val();
+
+    // Retrieving the movies
+    getMovies(searchText)
+      .then((movies) => {
+        // Removing the previous elements
+        $('#movies').empty();
+
+        // Displaying the movies 
+        $.each(movies, (index, movie) => {
+          let output = `
+          <div class='col-md-3'>
+            <div class='well text-center'>
+              <img src='${movie.Poster}'>
+              <h5>${movie.Title}</h5>
+            </div>
+          </div>
+          `;
+
+          $('#movies').append(output);
+        });
+      });
+  });
 });
+
+function getMovies(searchText) {
+  // Construct the endpoint
+  const API_KEY = '5d88bb13';
+  const baseURL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+  const fullURL = `${baseURL}s=${searchText}`;
+  console.log(fullURL);
+
+  // Request to the API
+  let results = axios.get(fullURL)
+    .then((response) => {
+      return response.data.Search;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return results;
+}
